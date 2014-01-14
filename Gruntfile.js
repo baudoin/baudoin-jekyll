88ef4dec5843e4b58482f4e842a0d6fdd06525f6
Andy Baudoin <andy@emergeinteractive.com>
2014-01-14 15:25:57 -0800

    edited gruntfile to pull css and js dirs in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,6 +256,8 @@ module.exports = function (grunt) {
             // Copy moves asset files and directories.
             'img/**/*',
             'fonts/**/*',
+            'css/**/*',
+            'js/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
